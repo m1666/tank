cdede9141691f32defaba9bcc63fd1577b8f9848
Mao <160663534@qq.com>
2020-07-06 13:58:56 +0800

    第一堂课，创建一个窗口

---
+++ src/com/mann/tank/Demo.java
@@ -0,0 +1,30 @@
+package com.mann.tank;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Frame f = new Frame();
+
+		// 创建窗口
+		f.setSize(800, 600);
+		f.setResizable(false);
+		f.setTitle("tank war");
+		f.setVisible(true);
+
+		// 创建监听器
+		f.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// 当点击叉叉的时候，退出系统
+				System.exit(0);
+			}
+
+		});
+	}
+
+}
